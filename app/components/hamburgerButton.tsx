0045b2cfeec3baf94eac44e9420f84d73e038e8f
Everts Rozenbaums <evertrozenbaum@gmail.com>
2024-09-23 11:11:42 +0300

    backup version prior next update

--- app/components/hamburgerButton.tsx
+++ app/components/hamburgerButton.tsx
@@ -5,37 +5,33 @@ import React from "react";
 export default function HamburgerButton() {
   return (
     <div className="relative">
-      {/* <button className="group relative">
-        <div className="relative flex h-[50px] w-[50px] transform items-center justify-center overflow-hidden rounded-full shadow-md ring-0 ring-gray-300 ring-opacity-30 transition-all duration-200 hover:ring-8 group-focus:ring-4">
-          <div className="flex h-[20px] w-[20px] origin-center transform flex-col justify-between overflow-hidden transition-all duration-300">
-            <div className="h-[2px] w-7 origin-left transform bg-white transition-all duration-300 group-focus:translate-x-10"></div>
-            <div className="h-[2px] w-7 transform rounded bg-white transition-all delay-75 duration-300 group-focus:translate-x-10"></div>
-            <div className="h-[2px] w-7 origin-left transform bg-white transition-all delay-150 duration-300 group-focus:translate-x-10"></div>
+      {/* Checkbox (Peer element) */}
+      <input
+        type="checkbox"
+        id="menu-toggle"
+        className="peer hidden"
+        onMouseDown={() => console.log("skibidi")}
+      />
 
-            <div className="absolute top-2.5 flex w-0 -translate-x-10 transform items-center justify-between transition-all duration-500 group-focus:w-12 group-focus:translate-x-0">
-              <div className="absolute h-[2px] w-5 rotate-0 transform bg-white transition-all delay-300 duration-500 group-focus:rotate-45"></div>
-              <div className="absolute h-[2px] w-5 -rotate-0 transform bg-white transition-all delay-300 duration-500 group-focus:-rotate-45"></div>
-            </div>
-          </div>
-        </div>
-      </button> */}
-      <input type="checkbox" id="menu-toggle" className="peer hidden" />
-
+      {/* Hamburger Button */}
       <label htmlFor="menu-toggle" className="group relative cursor-pointer">
-        <div className="relative flex h-[50px] w-[50px] transform items-center justify-center overflow-hidden rounded-full shadow-md ring-0 ring-gray-300 ring-opacity-30 transition-all duration-200 hover:ring-8 peer-focus:ring-4">
+        <div className="relative flex h-[50px] w-[50px] transform items-center justify-center overflow-hidden rounded-full shadow-md ring-0 ring-gray-300 ring-opacity-30 transition-all duration-200 hover:ring-8">
+          {/* Hamburger Icon Lines */}
           <div className="flex h-[20px] w-[20px] origin-center transform flex-col justify-between overflow-hidden transition-all duration-300">
-            <div className="h-[2px] w-7 origin-left transform bg-white transition-all duration-300 group-focus:translate-x-10"></div>
-            <div className="h-[2px] w-7 transform rounded bg-white transition-all delay-75 duration-300 group-focus:translate-x-10"></div>
-            <div className="h-[2px] w-7 origin-left transform bg-white transition-all delay-150 duration-300 group-focus:translate-x-10"></div>
+            {/* Top Line: Rotate on peer-checked */}
+            <div className="h-[2px] w-7 origin-left transform bg-white transition-all duration-300 peer-checked:translate-y-2 peer-checked:rotate-45"></div>
 
-            <div className="absolute top-2.5 flex w-0 -translate-x-10 transform items-center justify-between transition-all duration-500 group-focus:w-12 group-focus:translate-x-0">
-              <div className="absolute h-[2px] w-5 rotate-0 transform bg-white transition-all delay-300 duration-500 group-focus:rotate-45"></div>
-              <div className="absolute h-[2px] w-5 -rotate-0 transform bg-white transition-all delay-300 duration-500 group-focus:-rotate-45"></div>
-            </div>
+            {/* Middle Line: Disappear on peer-checked */}
+            <div className="h-[2px] w-7 transform rounded bg-white transition-all delay-75 duration-300 peer-checked:opacity-0"></div>
+
+            {/* Bottom Line: Rotate on peer-checked */}
+            <div className="h-[2px] w-7 origin-left transform bg-white transition-all delay-150 duration-300 peer-checked:-translate-y-2 peer-checked:-rotate-45"></div>
           </div>
         </div>
       </label>
-      <div className="peer-not-checked:translate-y-[-10px] peer-not-checked:opacity-0 peer-not-checked:duration-300 absolute right-0 top-[120%] mt-2 h-96 w-52 translate-y-[-10px] bg-white opacity-0 shadow-lg transition-all duration-300 peer-checked:translate-y-0 peer-checked:opacity-100 peer-checked:delay-100 peer-checked:duration-300">
+
+      {/* Dropdown Menu */}
+      <div className="peer-not-checked:translate-y-[-10px] peer-not-checked:opacity-0 absolute right-0 top-[120%] mt-2 h-96 w-52 translate-y-[-10px] rounded-3xl bg-white opacity-0 shadow-lg transition-all duration-300 peer-checked:translate-y-0 peer-checked:opacity-100">
         <menu className="space-y-2 p-4 text-black">
           <li>lorem</li>
           <li>lorem</li>
